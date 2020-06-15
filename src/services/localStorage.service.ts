5d03eb3f8defb3b35f8b0cb09f506ccbb76d28e0
himohimo <himohimo@gmail.com>
2020-06-14 21:04:21 -0700

    Adds localstorage service

---
+++ src/services/localStorage.service.ts
@@ -0,0 +1,40 @@
+enum LocalStorageKeyEnum {
+  RecentlyUsedIds = '_recently_used_id'
+}
+
+class LocalStorageService {
+
+  private _isLocalStorageAvailable(){
+      const testKey = '___TEST___';
+      try {
+          localStorage.setItem(testKey, testKey);
+          localStorage.removeItem(testKey);
+          return true;
+      } catch(err) {
+          return false;
+      }
+  }
+
+  get (key: LocalStorageKeyEnum): string {
+    if (!this._isLocalStorageAvailable) { return ''; }
+
+    try {
+      return localStorage.getItem(key) || '';
+    } catch (error) {
+      return '';
+    }
+  }
+
+  set (key: LocalStorageKeyEnum, value: string): boolean {
+    if (!this._isLocalStorageAvailable) { return false; }
+
+    try {
+      localStorage.setItem(key, value);
+      return true;
+    } catch (error) {
+      return false;
+    } 
+  }
+}
+
+export default new LocalStorageService();
