049e50550b765ba01bdf54ed0afca8c4e6bd7828
Andrew Nash <akahadaka@gmail.com>
2020-06-17 14:55:16 -0700

    fix: Move Firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,17 +1,7 @@
 import * as firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-
-export const firebaseConfig = {
-  apiKey: 'AIzaSyBmlYknpEzdc0D5gEtw0DUaoVq3OWPzA28',
-  authDomain: 'gfn-games.firebaseapp.com',
-  databaseURL: 'https://gfn-games.firebaseio.com',
-  projectId: 'gfn-games',
-  storageBucket: 'gfn-games.appspot.com',
-  messagingSenderId: '128823013918',
-  appId: '1:128823013918:web:c56408ef8452262d43340f',
-  measurementId: 'G-WLZJCZ476E',
-};
+import firebaseConfig from './firebase.config.json';
 
 firebase.initializeApp(firebaseConfig);
 
